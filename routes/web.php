24dfb2299250e0104c45c3a8c90ecb4fbe335cd9
gibransebagaisaturajp <gibransebagai2@gmail.com>
2019-06-22 16:21:52 +0700

    satura

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,33 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
+
+Route::get('welcome', function () {
+    return view('welcome');
+});
+Route::get('admin', function () {
+    return view('backend');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/single', function () {
+    return view('single');
+});
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+Route::get('/news', function () {
+    return view('news');
+});
+Route::get('/elements', function () {
+    return view('elements');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/causes', function () {
+    return view('contact');
+});
